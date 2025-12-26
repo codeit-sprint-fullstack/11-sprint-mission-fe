ce4f76e6b358504a31a0c31355f1802f25bf6fc8
yyejin00 <bywlslgnsl20@gmail.com>
2025-12-26 23:01:27 +0900

    fix : ArticleService 에서 then,catch 메서드 변경

--- api/articleService.js
+++ api/articleService.js
@@ -1,23 +1,29 @@
 export const getArticleList = async ({ page, pageSize, keyword }) => {
-  try {
-    const params = new URLSearchParams({
-      page,
-      pageSize,
-      ...(keyword&&{keyword:keyword}) , 
-    });
-    const response = await fetch(
-      `https://panda-market-api-crud.vercel.app/articles?${params.toString()}`
+  const params = new URLSearchParams({
+    page,
+    pageSize,
+    ...(keyword && { keyword: keyword }),
+  });
+  fetch(
+    `https://panda-market-api-crud.vercel.app/articles?${params.toString()}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        console.log(
+          `article getList - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+        );
+        return;
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) =>
+      console.log(` article getList - 에러발생${error.message}`)
     );
-    if (!response.ok) {
-      console.log(`getList - 정보를 불러올 수 없음. 상태코드 : ${response.status}`);
-      return;
-    }
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log(`에러발생${error.message}`);
-  }
 };
 
 // const articleQuery = {
@@ -28,93 +34,100 @@ export const getArticleList = async ({ page, pageSize, keyword }) => {
 // getArticleList(articleQuery);//잘나옴
 
 export const getArticle = async ({ id }) => {
-  try {
-    const response = await fetch(
-      `https://panda-market-api-crud.vercel.app/articles/${id}`
-    );
-    if (!response.ok) {
-      console.log(`get - 정보를 불러올 수 없음. 상태코드 : ${response.status}`);
-      return;
-    }
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch(error) {
-    console.log(`에러발생${error.message}`);
-  }
+  fetch(`https://panda-market-api-crud.vercel.app/articles/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        console.log(
+          `article get - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+        );
+        return;
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      console.log(`articel 에러발생${error.message}`);
+    });
 };
 // getArticle({id:5494}); //잘나옴!
 
 export const createArticle = async ({ title, content, image }) => {
-  try{
-const response = await fetch(
-    'https://panda-market-api-crud.vercel.app/articles',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title,
-        content,
-        image,
-      }),
-    }
-  );
-  if (!response.ok) {
-      console.log(`create - 정보를 불러올 수 없음. 상태코드 : ${response.status}`);
-      return;
-    }
-    const data = await response.json();
-    console.log(data);
-    return data;
-  }catch{
-    
-  }
-  
-
+  fetch('https://panda-market-api-crud.vercel.app/articles', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title,
+      content,
+      image,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log(
+          `create - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+        );
+        return;
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) =>
+      console.log(` article create - 에러발생${error.message}`)
+    );
 };
 
 const patchArticle = async (articleId, patchData) => {
-  try{
-const response = await fetch(
-    `https://panda-market-api-crud.vercel.app/articles/${articleId}`,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(patchData),
-    }
-    
-  );
-  if(!response.ok){
-      console.log(`patch - 정보를 불러올 수 없음. 상태코드 : ${response.status}`);
-    }
-  }catch{}
-
-  const data = await response.json();
-  console.log(data);
-  return data;
-};
-export const deleteArticle = async ({articleId}) => {
-  try{
-const response = await fetch(
-    `https://panda-market-api-crud.vercel.app/articles/${articleId}`,{
-      method: 'DELETE',
-      headers:{
-        'Content-Type': 'application/json',
-      },
-    }
-  );
-  if(!response.ok){
-       console.log(`delete - 정보를 불러올 수 없음. 상태코드 : ${response.status}`);
-      return;
-  }
-
-  }catch(error){
-    console.log(`에러발생${error.message}`);
-  }
+  fetch(`https://panda-market-api-crud.vercel.app/articles/${articleId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(patchData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log(
+          `patch - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+        );
+        return;
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => console.log(` article patch - 에러발생${error.message}`));
 };
 
-
+export const deleteArticle = async ({ articleId }) => {
+  fetch(`https://panda-market-api-crud.vercel.app/articles/${articleId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log(
+          `delete - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+        );
+        return;
+      } else {
+        console.log(response.status);
+        return response.status;
+      }
+    })
+    .catch((error) => console.log(`article delete - 에러발생${error.message}`));
+};
