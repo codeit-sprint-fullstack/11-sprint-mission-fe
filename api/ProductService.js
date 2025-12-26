20617ab1115c39b62f34f8bf5a30ec583b168939
yyejin00 <bywlslgnsl20@gmail.com>
2025-12-26 23:18:37 +0900

    fix : product 에러코드 부분 수정

--- api/ProductService.js
+++ api/ProductService.js
@@ -9,16 +9,16 @@ export const getProductList = async ({ page, pageSize, keyword }) => {
       `https://panda-market-api-crud.vercel.app/products?${params.toString()}`
     );
     if (!response.ok) {
-      console.log(
-        `getList - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+      throw new Error(
+        `[product getList] - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
       );
-      return;
     }
     const data = await response.json();
     console.log(data);
     return data;
   } catch (error) {
-    console.log(`getList - 에러발생${error.message}`);
+    console.log(`[product getList] - 에러발생${error.message}`);
+    throw error;
   }
 };
 
@@ -28,18 +28,26 @@ export const getProduct = async ({ id }) => {
       `https://panda-market-api-crud.vercel.app/products/${id}`
     );
     if (!response.ok) {
-      console.log(`get - 정보를 불러올 수 없음. 상태코드 : ${response.status}`);
-      return;
+      throw new Error(
+        `[product get] - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+      );
     }
     const data = await response.json();
     console.log(data);
     return data;
   } catch (error) {
-    console.log(`get - 에러발생${error.message}`);
+    console.log(`[product get] - 에러발생${error.message}`);
+    throw error;
   }
 };
 
-export const createArticle = async ({ name, description, price, tag, images }) => {
+export const createProduct = async ({
+  name,
+  description,
+  price,
+  tag,
+  images,
+}) => {
   try {
     const response = await fetch(
       'https://panda-market-api-crud.vercel.app/products',
@@ -58,20 +66,20 @@ export const createArticle = async ({ name, description, price, tag, images }) =
       }
     );
     if (!response.ok) {
-      console.log(
-        `create - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+      throw new Error(
+        `[product create] - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
       );
-      return;
     }
     const data = await response.json();
     console.log(data);
     return data;
   } catch (error) {
-    console.log(`create 에러발생 ${error.message}`);
+    console.log(`[product create] - 에러발생 ${error.message}`);
+    throw error;
   }
 };
 
-const patchProducts = async (productId, patchData) => {
+export const patchProducts = async (productId, patchData) => {
   try {
     const response = await fetch(
       `https://panda-market-api-crud.vercel.app/products/${productId}`,
@@ -84,35 +92,36 @@ const patchProducts = async (productId, patchData) => {
       }
     );
     if (!response.ok) {
-      console.log(
-        `patch - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+      throw new Error(
+        `[product patch] - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
       );
     }
     const data = await response.json();
     console.log(data);
     return data;
   } catch (error) {
-    console.log(`patch - 에러발생${error.message}`);
+    console.log(`[product patch] - 에러발생${error.message}`);
+    throw error;
   }
 };
+
 export const deleteProduct = async ({ productId }) => {
   try {
     const response = await fetch(
       `https://panda-market-api-crud.vercel.app/products/${productId}`,
       {
         method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
       }
     );
     if (!response.ok) {
-      console.log(
-        `delete - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
+      throw new Error(
+        `[product delete] - 정보를 불러올 수 없음. 상태코드 : ${response.status}`
       );
-      return;
+    } else {
+      return response.status;
     }
   } catch (error) {
-    console.log(`에러발생${error.message}`);
+    console.log(`[product delete] - 에러발생${error.message}`);
+    throw error;
   }
 };
