d611f8e8e265f9621e1f3d13f12698e3b24d03be
neraziim <neraziim@gmail.com>
2026-01-03 10:00:59 +0900

    feat: migrate API services and preserve legacy main.js logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*main.js의 내용을 옮겨 옴
+import {
+  createArticle,
+  deleteArticle,
+  getArticle,
+  getArticleList,
+  patchArticle,
+} from './ArticleService.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  createProduct,
+  deleteProduct,
+  getProduct,
+  getProductList,
+  patchProduct,
+} from './ProductService.js';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+//articles test
+const newArticle = {
+  image: 'https://example.com/...',
+  content: '게시글 내용입니까?',
+  title: '게시글 제목입니까?',
+};
+const fixArticle = {
+  image: 'https://example.com/...',
+  content: '게시글 내용입니다!!!',
+  title: '게시글 제목입니다!!!!!',
+};
+getArticleList().then((x) => console.log(x));
+getArticle(5516).then((x) => console.log(x));
+createArticle( newArticle).then((x) => console.log(x));
+patchArticle( 5523,fixArticle).then((x) => console.log(x));;
+deleteArticle( 5522);
+getArticle( 5522).then((x) => console.log(x));
+
+//products test
+const newProduct = {
+  images: ['https://example.com/...'],
+  tags: ['학용품'],
+  price: 30000,
+  description: 'string',
+  name: '고급 샤프',
+};
+const fixProduct = {
+  images: ['https://example.com/...'],
+  tags: ['학용품'],
+  price: 3000,
+  description: 'string',
+  name: '샤프',
+};
+
+getProductList().then((r) => console.log(r)); //test
+getProduct(1800).then((r) => console.log(r)); //test
+createProduct(newProduct).then((r) => console.log(r)); //test
+patchProduct( 2940, fixProduct).then((r) => console.log(r)); //test
+// delete 확인
+deleteProduct(2940).then((r) => console.log(r));//test
+getProduct( 2938).then((r) => console.log(r)); //test
+*/
