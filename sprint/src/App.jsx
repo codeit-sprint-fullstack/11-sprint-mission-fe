e65de76da72faa12819831da1d4a943818e4ebec
ZoeYoon <bus1990@naver.com>
2026-01-03 20:52:41 +0900

    중간 commit

--- sprint/src/App.jsx
+++ sprint/src/App.jsx
@@ -1,5 +1,8 @@
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
+import BestItem from "./context/BestItem"
+import ProductList from "./context/ProductList"
+
 import styles from "./App.module.css"
 
 function App() {
@@ -9,7 +12,20 @@ function App() {
         <Header />
 
         <main>
-          <h1>중고마켓</h1>
+
+          <section>
+            <div>
+              <h2>베스트 상품</h2>
+              <BestItem/>
+            </div>
+          </section>
+
+          <section>
+            <div>
+              <h2>판매 중인 상품</h2>
+              <ProductList/>
+            </div>
+          </section>
         </main>
         
         <Footer />
---
+++ sprint/src/context/ProductList.jsx
@@ -0,0 +1,6 @@
+
+function ProductList (){
+  return
+}
+
+export default ProductList;
