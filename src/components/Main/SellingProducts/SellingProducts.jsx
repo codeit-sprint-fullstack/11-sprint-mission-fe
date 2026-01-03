517a2570b339f09263b48e8d6ee98db394dfcbe1
eungyeolbaek <eungyeolbaek@gmail.com>
2026-01-04 03:10:43 +0900

    feat: implement pagination in SellingProducts

--- src/components/Main/SellingProducts/SellingProducts.jsx
+++ src/components/Main/SellingProducts/SellingProducts.jsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from 'react';
+import { usePagination } from '@/hooks/usePagination';
 import { getProductList } from '@/apis/fetchProducts';
+import { getPageButtons } from '@/utils/getPageButtons';
 import { SearchProducts } from './SearchProducts';
 import { Product } from '@/components/common/Product';
 import styles from './SellingProducts.module.css';
+import { Pagination } from './Pagination';
 
 export function SellingProducts() {
   const [products, setProducts] = useState([]);
 
+  const { currentPage, totalPages, setTotalItems, goToPage, next, prev } =
+    usePagination();
+
   useEffect(() => {
     async function fetchProducts() {
-      const result = await getProductList({});
-      setProducts(result.list);
-      //   배열 이름으로 받아오기
+      const result = await getProductList({
+        page: currentPage,
+      });
+      //   객체 프로퍼티 이름으로 가져오기!
+      setProducts(result.list); // 프로덕트 배열 가져옴
+      setTotalItems(result.totalCount); // 전체 프로덕트 개수
     }
 
     fetchProducts();
-  }, []);
+  }, [currentPage, setTotalItems]); // setTotalItems는 함수라서 무한루프 x
+
+  const pageButtons = getPageButtons(currentPage, totalPages);
 
   return (
     <section className={styles.section}>
@@ -23,7 +34,7 @@ export function SellingProducts() {
         <h2 className={styles.productsTitle}>판매 중인 상품</h2>
         <SearchProducts />
       </div>
-      
+
       <ul className={styles.sellingProductList}>
         {products.map(({ id, images, name, price, favoriteCount }) => (
           <li key={id}>
@@ -37,6 +48,16 @@ export function SellingProducts() {
           </li>
         ))}
       </ul>
+
+      <Pagination
+        pageButtons={pageButtons}
+        goToPage={goToPage}
+        next={next}
+        prev={prev}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        className={styles.pagination}
+      />
     </section>
   );
 }
