bf718020080993f06a4e987e5b7051aa4662ba40
hyojin-10 <wowshally@gmail.com>
2026-01-04 20:52:48 +0900

    feat: usePagination 훅 구현

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export const usePagination = (initialPage = 1, itemsPerPage = 10) => {
+  const [currentPage, setCurrentPage] = useState(initialPage);
+  const [totalItems, setTotalItems] = useState(0);
+
+  const totalPages = Math.ceil(totalItems / itemsPerPage) || 1;
+
+  const goToPage = (pageNumber) => {
+    if (pageNumber <= 0 || pageNumber > totalPages) return;
+    setCurrentPage(pageNumber);
+  };
+
+  const next = () => goToPage(currentPage + 1);
+  const prev = () => goToPage(currentPage - 1);
+
+  return {
+    currentPage,
+    totalPages,
+    setTotalItems,
+    goToPage,
+    next,
+    prev,
+    setCurrentPage,
+  };
+};
