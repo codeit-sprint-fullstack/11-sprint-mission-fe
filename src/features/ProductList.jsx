24b1f68d756be25ee02e24d8be78d635c5f03f96
hyojin-10 <wowshally@gmail.com>
2026-01-04 18:24:06 +0900

    feat: 판매 중인 상품 섹션 구현 및 API 연동

--- src/features/ProductList.jsx
+++ src/features/ProductList.jsx
@@ -1,23 +1,68 @@
+import { useState, useEffect } from 'react';
+import { getProductList } from '@/api/productApi';
+import { useDeviceType } from '@/hooks/useDeviceType';
+import ProductCard from '@/components/ProductCard';
 import './ProductList.css';
 
 function ProductList() {
-    return (
-        <section className='list-section'>
-            <div className='sectoin-wrapper'>
-                <div className='section-header'>
-                    <h2>판매 중인 상품</h2>
-                    <div className='section-nav'>
-                        <input type='text' placeholder='검색할 상품을 입력해주세요' className="item-search"></input>
-                        <button className="button registration-button">상품 등록하기</button>
-                        <div className="sort-dropdown">최신순</div>
-                    </div>
-                </div>
-                <div className='list-container'>
-                    <div>상품</div>
-                </div>
-            </div>
-        </section>
-    )
+  const [products, setProducts] = useState([]);
+  const [orderBy, setOrderBy] = useState('recent');
+  const [keyword, setKeyword] = useState('');
+  const device = useDeviceType();
+
+  const pageSize = device === 'mobile' ? 4 : device === 'tablet' ? 6 : 10;
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const items = await getProductList({
+          page: 1,
+          pageSize: pageSize,
+          orderBy: orderBy,
+          keyword: keyword,
+        });
+        setProducts(items.list);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    };
+
+    getProducts();
+  }, [pageSize, orderBy, keyword]);
+
+  return (
+    <section className="list-section">
+      <div className="section-header">
+        <h2 className="section-title">판매 중인 상품</h2>
+        <div className="section-nav">
+          <input
+            type="text"
+            placeholder="검색할 상품을 입력해주세요"
+            className="item-search"
+            value={keyword}
+            onChange={(event) => setKeyword(event.target.value)}
+          ></input>
+          <button className="button registration-button">상품 등록하기</button>
+          <select
+            className="sort-dropdown"
+            value={orderBy}
+            onChange={(event) => setOrderBy(event.target.value)}
+          >
+            <option className="recent">최신순</option>
+            <option className="favorite">좋아요순</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="product-container">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} type="list" />
+        ))}
+      </div>
+
+      <div className="pagination-container"></div>
+    </section>
+  );
 }
 
 export default ProductList;
