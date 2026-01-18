aecd52c24d3274b74d49a6b31d2f467198c44902
eungyeolbaek <eungyeolbaek@gmail.com>
2026-01-18 10:20:52 +0900

    fix: ensure footer sticks to bottom on short pages

--- src/domains/Item/pages/ItemPage/ItemPage.jsx
+++ src/domains/Item/pages/ItemPage/ItemPage.jsx
@@ -6,15 +6,17 @@ import styles from './ItemPage.module.css';
 
 export function ItemPage() {
   return (
-    <>
+    <div className={styles.container}>
       <Header title="판다마켓 중고마켓 페이지">
         <Menu>자유게시판</Menu>
         <Menu className={styles.color}>중고마켓</Menu>
       </Header>
 
-      <SellingProductsSection />
+      <div className={styles.main}>
+        <SellingProductsSection />
+      </div>
 
       <Footer />
-    </>
+    </div>
   );
 }
--- src/domains/Registration/pages/RegistrationPage/RegistrationPage.jsx
+++ src/domains/Registration/pages/RegistrationPage/RegistrationPage.jsx
@@ -2,18 +2,21 @@ import { Header } from '@/components/Header';
 import { Menu } from '@/components/Menu';
 import { RegistrationFormSection } from '../../components/RegistrationFormSection';
 import { Footer } from '@/components/Footer';
+import styles from './RegistrationPage.module.css';
 
 export function RegistrationPage() {
   return (
-    <>
+    <div className={styles.container}>
       <Header title="판다마켓 중고마켓 페이지">
         <Menu>자유게시판</Menu>
         <Menu>중고마켓</Menu>
       </Header>
 
-      <RegistrationFormSection />
+      <div className={styles.main}>
+        <RegistrationFormSection />
+      </div>
 
       <Footer />
-    </>
+    </div>
   );
 }
