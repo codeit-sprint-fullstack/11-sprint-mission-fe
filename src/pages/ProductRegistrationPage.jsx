3a4bd11cc755d9c0ce0d47f3589ad187d5d17bee
hyojin-10 <wowshally@gmail.com>
2026-01-18 16:38:51 +0900

    feat: 태그 칩 생성/삭제 기능 구현

--- src/pages/ProductRegistrationPage.jsx
+++ src/pages/ProductRegistrationPage.jsx
@@ -1,18 +1,39 @@
 import InputBox from '@/components/InputBox';
 import { useState } from 'react';
 import { useProductValidation } from '@/hooks/useProductValidation';
+import { IoIosCloseCircle } from 'react-icons/io';
 
 function ProductRegistrationPage() {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
   const [tag, setTag] = useState('');
+  const [tags, setTags] = useState([]);
 
   const { errors, validate } = useProductValidation();
 
   const handleInputChange = (type, value, change) => {
     change(value);
     validate(type, value);
+  };
+
+  // 태그 입력 후 엔터 시 칩 생성
+  const handleKeyDown = (e) => {
+    if (e.nativeEvent.isComposing) return; // 한글 마지막 글자 두번씩 저장되는거 방지
+
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      if (tag.trim() !== '' && !errors.tag) {
+        setTags([...tags, tag]);
+        setTag('');
+      }
+    }
+  };
+
+  // 칩에서 x 클릭 시 삭제
+  const handleDeleteTag = (deleteTag) => {
+    const newTags = tags.filter((_, i) => i !== deleteTag);
+    setTags(newTags);
   };
 
   return (
@@ -66,13 +87,26 @@ function ProductRegistrationPage() {
         <InputBox
           label="태그"
           guideText="5자 이내로 입력해주세요"
-          errorMessage={errors.tags}
+          errorMessage={errors.tag}
         >
           <input
             placeholder="태그를 입력해주세요"
             value={tag}
             onChange={(e) => handleInputChange('tag', e.target.value, setTag)}
+            onKeyDown={handleKeyDown}
           />
+
+          <div className="tag-list">
+            {tags.map((tag, i) => (
+              <span key={i} className="tag-chip">
+                #{tag}
+                <IoIosCloseCircle
+                  className="tag=delete"
+                  onClick={() => handleDeleteTag(i)}
+                />
+              </span>
+            ))}
+          </div>
         </InputBox>
       </div>
     </div>
