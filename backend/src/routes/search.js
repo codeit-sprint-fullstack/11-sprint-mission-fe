d7bb47745cab86e71ee46930aabbf3eb5ae8485a
DevWoojin97 <dnekdnwls@gmail.com>
2026-01-20 11:52:27 +0900

    백엔드 분리

--- backend/src/routes/search.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-export const searchRouter = express.Router();
-
-searchRouter.get('/', (req, res) => {
-  const { q, limit = 10 } = req.query;
-  res.json({ query: q, limit: Number(limit) });
-});
